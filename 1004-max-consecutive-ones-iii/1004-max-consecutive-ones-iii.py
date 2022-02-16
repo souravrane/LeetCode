4b3c03cecc8d99e4966d8d86c03d267dac72c2c2
Sourav Rane <sourav1121997@gmail.com>
2022-02-16 20:33:55 +0530

    Time: 898 ms (37.92%), Space: 14.5 MB (99.53%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
+        n = len(nums)
+        max_distance = 0
+        
+        i,j = 0,0
+        zero_count = 0
+        
+        # The idea is to have a sliding window which always has atmost k zeroes inside it
+        while j < n:
+            
+            if nums[j] == 0:
+                zero_count += 1
+            
+            # if the number of zeroes exceed k inside our window 
+            # we start reducing the window to the point where only k zeroes are present.
+            if zero_count > k:
+                while zero_count > k:
+                    if nums[i] == 0:
+                        zero_count -= 1
+                    i += 1
+                
+            if zero_count <= k:
+                # now our window has k zeroes we find the distance
+                window_size = j-i+1
+                max_distance = max(max_distance, window_size)
+            
+            j += 1
+                
+
+        return max_distance
+            
+            
+            
+            
+          
+                        
+                
+            
+        
+                        
+                
+                
+                
+                    
+        
+        
