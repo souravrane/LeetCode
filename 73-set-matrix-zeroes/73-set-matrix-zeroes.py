b76139b42b0b9f43ab7326f4ef8f52c05bb50a5a
Sourav Rane <sourav1121997@gmail.com>
2022-02-17 10:37:42 +0530

    Time: 214 ms (29.13%), Space: 14.7 MB (97.60%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        '''
         row = set()
         col = set()
         
@@ -22,6 +23,36 @@ class Solution:
         for x in col:
             for j in range(r):
                 matrix[j][x] = 0
+        
+        '''
+        # The above solution uses T.C = O(mn) , S.C = O(m+n)
+        
+        # The below solution uses T.C = O(mn) , S.C = O(1)
+        r = len(matrix)
+        c = len(matrix[0])
+
+        # mark all the rows with zeroes to None
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] == 0:
+                    for k in range(c):
+                        if k != j and matrix[i][k] != 0: matrix[i][k] = None
+        
+        # mark all the colum with zeroes to None
+        for i in range(c):
+            for j in range(r):
+                if matrix[j][i] == 0:
+                    for k in range(r):
+                        if k != j and matrix[k][i] != 0: matrix[k][i] = None
+                            
+        # mark all Nones to 0s
+        for i in range(c):
+            for j in range(r):
+                if matrix[j][i] == None:
+                    matrix[j][i] = 0
+        
+        
+
         
                 
         
