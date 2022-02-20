d0d115d815c2be842da217cdacbf9eea1bc98486
Sourav Rane <sourav1121997@gmail.com>
2022-02-20 13:34:25 +0530

    Time: 171 ms (25.20%), Space: 14.4 MB (96.36%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,31 @@
+from functools import cmp_to_key
+
+def compare(a,b):
+    if a[0] < b[0]:
+        return -1   
+    return 1
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals = sorted(intervals, key=cmp_to_key(compare))
+        print(intervals)
+        left,right = intervals[0]
+        count = 0
+        for i in range(1,len(intervals)):
+            curr = intervals[i]
+            if curr[0] >= left and curr[1] <= right:
+                count += 1
+            elif curr[0] == left and curr[1] > right:
+                count += 1
+                right = max(right,curr[1])
+            else:
+                left = curr[0]
+                right = max(right,curr[1])
+            
+        return len(intervals) - count
+
+                
+                
+                
+                
+                
