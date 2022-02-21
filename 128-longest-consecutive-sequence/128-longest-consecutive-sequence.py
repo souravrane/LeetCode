9d00088ddccda4a8ef95c54207c834160f8bd45a
Sourav Rane <sourav1121997@gmail.com>
2022-02-21 18:06:10 +0530

    Time: 200 ms (86.88%), Space: 27.7 MB (37.65%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,8 +4,10 @@ class Solution:
         
         longest = 0
         for n in numSet:
+            #  we check for the left value because that marks the start of a new sequence
             if (n - 1) not in numSet:
                 count = 0
+                # check how many consecutive numbers exist
                 while (n + count) in numSet:
                     count += 1
                 
