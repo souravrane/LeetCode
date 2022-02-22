cec5b92ae0540a3a3436cf6b3e1b6c646108762a
Sourav Rane <sourav1121997@gmail.com>
2022-02-22 19:53:01 +0530

    Time: 4084 ms (5.09%), Space: 32.3 MB (58.05%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def maximise(self,n,b):
+        shift = 0
+        res = 0
+        while shift < b:
+            if n & (1 << shift) == 0:
+                res = res | (1 << shift)
+            shift += 1
+        return res
+        
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:        
+        # calculate the prefix xor
+        prefix_xor = [nums[0]]
+        for i in range(1,len(nums)):
+            prefix_xor.append(prefix_xor[i-1] ^ nums[i])
+                
+        res = []
+        for i in range(len(nums)-1,-1,-1):
+            k = self.maximise(prefix_xor[i],maximumBit)
+            res.append(k)
+        
+        return res
+        
