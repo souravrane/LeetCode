9adfb7a5d976b0169f0be43d3ba3cccb8701d8a0
Sourav Rane <sourav1121997@gmail.com>
2022-02-23 14:19:15 +0530

    Time: 61 ms (10.40%), Space: 13.8 MB (84.48%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,21 +1,26 @@
+
+
+'''
+Method 1 : space: O(N) Time: O(N)
+Everytime we encounter a closing bracket in the stack, the height of the stack at that point
+will tell us what level of nesting was present so far before we pop it off.
+
+Method 2: space: O(1) Time: O(N)
+When we find left bracket add 1 to the count, right bracket subtract 1 from the count.
+Keep updating the global max everytime you count a left bracket. The answer would be the maximum
+left brackets at any given point.
+
+'''
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack = []
-        top = -1
+        
+        bracket_count = 0
         max_depth = 0
-        
-        '''
-        everytime we encounter a closing bracket, the height of the stack at that point
-        will tell us what level of nesting was present so far.
-        
-        '''
         for char in s:
             if char == "(":
-                stack.append(char)
-                top += 1
+                bracket_count += 1
+                max_depth = max(bracket_count,max_depth)
             elif char == ")":
-                max_depth = max(max_depth,top+1)
-                stack.pop()
-                top -= 1
+                bracket_count -= 1
         
         return max_depth
