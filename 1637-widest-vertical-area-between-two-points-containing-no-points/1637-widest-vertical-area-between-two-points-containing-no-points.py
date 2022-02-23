b426625e6afa361f797842b0dba6f8a64cf0c77b
Sourav Rane <sourav1121997@gmail.com>
2022-02-23 18:03:37 +0530

    Time: 1534 ms (14.21%), Space: 53.9 MB (98.32%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
@@ -1,18 +1,17 @@
-from functools import cmp_to_key
-def compare(a,b):
-    if a[0] <= b[0]:
-        return -1
-    return 1
-
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        points = sorted(points, key=cmp_to_key(compare))
+        s = set()
+        for i in points:
+            s.add(i[0])
+        
+        s = sorted(list(s))
+        
         widest_area = 0
         
         # we just need to check the x axis regions
-        for i in range(1,len(points)):
-            p2 = points[i]
-            p1 = points[i-1]
-            widest_area = max(widest_area, p2[0]-p1[0])
+        for i in range(1,len(s)):
+            p2 = s[i]
+            p1 = s[i-1]
+            widest_area = max(widest_area, p2-p1)
             
         return widest_area
