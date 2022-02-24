14e5a3a62f2ed91b65d5405dd142267ae88cce17
Sourav Rane <sourav1121997@gmail.com>
2022-02-24 13:57:34 +0530

    Time: 79 ms (76.28%), Space: 14.6 MB (85.55%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -4,10 +4,8 @@ class Solution:
         The possible number of deci binary numbers to add 
         upto n will be equal to the highest digit in the number. 
         '''
-        max_digit = 1
-        for num in n:
-            max_digit = max(max_digit, int(num))
+        
             
-        return max_digit
+        return max(n)
         
         
