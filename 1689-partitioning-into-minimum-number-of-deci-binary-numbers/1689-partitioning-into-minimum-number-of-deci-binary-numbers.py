e7458d388fbc14c78b3cb8e5d0c2ed11eacd68a6
Sourav Rane <sourav1121997@gmail.com>
2022-02-24 13:58:06 +0530

    Time: 121 ms (52.77%), Space: 14.8 MB (55.33%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -4,8 +4,10 @@ class Solution:
         The possible number of deci binary numbers to add 
         upto n will be equal to the highest digit in the number. 
         '''
-        
-            
+        '''
+        Also I was today years old when i found out applying max on a string
+        in python will give you max value from it ! :D
+        '''
         return max(n)
         
         
