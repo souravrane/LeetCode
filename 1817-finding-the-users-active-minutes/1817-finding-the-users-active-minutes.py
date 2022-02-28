87636577362f4ea9f72df4f0cf47daf0ead83cbd
Sourav Rane <sourav1121997@gmail.com>
2022-02-28 20:09:57 +0530

    Time: 1044 ms (82.01%), Space: 25 MB (23.05%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -2,25 +2,22 @@ from collections import defaultdict
 
 class Solution:
     def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
-        idCount = {}
-        uam = defaultdict(int)
+        uam = {}
         
         for log in logs:
             Id = log[0]
             time = log[1]
             
-            if Id in idCount:
-                idCount[Id].add(time)
+            if Id in uam:
+                uam[Id].add(time)
             else:
-                idCount[Id] = set([time])
-        
-        for key in idCount:
-            x = len(idCount[key])
-            uam[x] += 1
-        
+                uam[Id] = set([time])
+                
         res = [0]*k
         for key in uam:
-            res[key-1] = uam[key]
-            
+            x = len(uam[key])
+            res[x-1] += 1
+        
+    
         return res
         
