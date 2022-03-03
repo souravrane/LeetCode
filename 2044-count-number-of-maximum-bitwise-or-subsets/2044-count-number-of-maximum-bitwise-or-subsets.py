51e3e0739ec7ed1c55af4b73640d81d3941b464c
Sourav Rane <sourav1121997@gmail.com>
2022-03-04 00:20:46 +0530

    Time: 2995 ms (19.28%), Space: 50.2 MB (5.72%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,17 +1,27 @@
 maxOR = 0
+dp = [0]
 
 class Solution:
     def subsetsOR(self,index,OR,arr):
         if index == len(arr):
             if OR == maxOR: return 1
             return 0
-        return self.subsetsOR(index+1, OR | arr[index],arr ) + self.subsetsOR(index+1, OR,arr )
-            
+        
+        if dp[index][OR] != -1:
+            return dp[index][OR]
+        
+        dp[index][OR] = self.subsetsOR(index+1, OR | arr[index],arr ) + self.subsetsOR(index+1, OR,arr )
+        return dp[index][OR]
+    
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         global maxOR
+        global dp
+        n = len(nums)
         maxOR = 0
         for num in nums:
             maxOR = maxOR | num
+        
+        dp = [[-1 for i in range(maxOR+1)] for j in range(n+1)]
         return self.subsetsOR(0,0,nums)
         
         
