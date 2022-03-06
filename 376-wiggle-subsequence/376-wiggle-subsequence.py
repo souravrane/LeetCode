0f04424957bc1a6ddf270747ad87b0885992e5cf
Sourav Rane <sourav1121997@gmail.com>
2022-03-06 21:54:15 +0530

    Time: 51 ms (57.89%), Space: 14 MB (49.51%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -6,6 +6,11 @@ class Solution:
         high = 1
         low = 1
         
+        '''
+        if the diff is postive then the previous number was supposed to be high , hence high = low + 1
+        if the diff is negative then the previous number was supposed to be low , hence low = high + 1
+
+        '''
         for i in range(1,n):
             diff = num[i] - num[i-1]
             if diff > 0:
