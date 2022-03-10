638ac83c42efbfc6bbc7a8e92dd1651c868df4bb
Sourav Rane <sourav1121997@gmail.com>
2022-03-10 19:26:27 +0530

    Time: 103 ms (43.68%), Space: 13.9 MB (65.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -8,40 +8,25 @@ class Solution:
         carry = 0
         head = ListNode(None)
         curr = head
-        while l1 != None and l2 != None:
-            s = (l1.val + l2.val + carry) % 10
-            carry = (l1.val + l2.val + carry) // 10
+        while l1 != None or l2 != None or carry:
+            num1,num2 = 0,0
             
+            if l1 != None:
+                num1 = l1.val
+                l1 = l1.next
+            
+            if l2 != None:
+                num2 = l2.val
+                l2 = l2.next
+            
+            s = (num1 + num2 + carry) % 10
+            carry = (num1 + num2 + carry) // 10
+ 
             newNode = ListNode(s)
             curr.next = newNode
             curr = newNode
-            
-            l1 = l1.next
-            l2 = l2.next
-        
-        while l1 != None :
-            s = (l1.val + carry) % 10
-            carry = (l1.val + carry) // 10
-            
-            newNode = ListNode(s)
-            curr.next = newNode
-            curr = newNode
-            
-            l1 = l1.next
-        
-        while l2 != None :
-            s = (l2.val + carry) % 10
-            carry = (l2.val + carry) // 10
-            
-            newNode = ListNode(s)
-            curr.next = newNode
-            curr = newNode
-            
-            l2 = l2.next
-            
-        if carry != 0:
-            newNode = ListNode(carry)
-            curr.next = newNode
+       
+       
         
         return head.next
             
