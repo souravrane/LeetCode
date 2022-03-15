cead208fda014ff7f3e07a45bb85adfa54fecb98
Sourav Rane <sourav1121997@gmail.com>
2022-03-15 17:25:08 +0530

    Time: 168 ms (72.00%), Space: 18.1 MB (86.14%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,48 +1,19 @@
 from functools import cmp_to_key
 
+def compare(a,b):
+    if a[0] > b[0]:
+        return 1
+    return -1
+
 class Solution:
-    def compare(self,a,b):
-        if a[0] < b[0]:
-            return -1
-        
-        if a[0] > b[0]:
-            return 1
-        
-        if a[1] < b[1]:
-            return -1
-        else:
-            return 1
-        
-    
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=cmp_to_key(self.compare))
-        res = []
-        n = len(intervals)
-        i,j=0,0
-        min_start , max_end = intervals[0]
-        '''
-        idea here is to cover up all the intervals in the window i - j 
-        which are overlapping. 
-        How to check if they overlap ? 
-        if the previous intervals end is greater than or equal to 
-        the next one's start, that means that they overlap
-        ''' 
-        while j < n:
-            if  j != (n-1) and (max_end >= intervals[j+1][0]):
-                j+=1
-                min_start = min(min_start,intervals[j][0])
-                max_end = max(max_end,intervals[j][1])
+        intervals = sorted(intervals,key=cmp_to_key(compare))
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
             else:
-                res.append([min_start,max_end])
-                j += 1
-                i = j
-                if j < n:
-                    min_start,max_end = intervals[j]
-                
-        return res
+                merged[-1][1] = max(merged[-1][1],interval[1])
         
-        
-        
-        
-        
+        return merged
         
