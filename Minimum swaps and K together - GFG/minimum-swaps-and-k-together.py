c38670ac8ff2afa7c37bc43637695af16fa395b9
Sourav Rane <sourav1121997@gmail.com>
2022-03-18 12:56:16 +0530

    Added solution - LeetHub

---
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+def minSwap (arr, n, k) : 
+    #Complete the function
+    windowSize = 0
+    for num in arr:
+        if num <= k: windowSize += 1
+    
+    currCount = 0
+    for i in range(windowSize):
+        if arr[i] <= k: currCount += 1
+    
+    maxCount = currCount
+    
+    if maxCount == windowSize: return 0
+    
+    for i in range(1,max(n-windowSize + 1,2)):
+        if arr[i-1] <= k:
+            currCount -= 1
+        
+        if arr[i+windowSize-1] <= k:
+            currCount += 1
+            
+        maxCount = max(maxCount,currCount)
+    
+    return windowSize - maxCount
+        
+    
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    k = int(input())
+    ans = minSwap(arr, n, k)
+    print(ans)
+    
+
+
+
+
+
+# } Driver Code Ends
