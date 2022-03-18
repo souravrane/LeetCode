6fefd26fbb50469c996a1bf82514f4d528b3494e
Sourav Rane <sourav1121997@gmail.com>
2022-03-18 12:24:48 +0530

    Time: 1995 ms (65.87%), Space: 24.6 MB (94.63%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -0,0 +1,38 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        rows = len(matrix)
+        columns = len(matrix[0])
+        
+        # row wise prefix
+        for i in range(rows):
+            for j in range(1,columns):
+                matrix[i][j] += matrix[i][j-1]
+        
+        #column wise prefix
+        for i in range(columns):
+            for j in range(1,rows):
+                matrix[j][i] += matrix[j-1][i]
+                
+        self.matrix = matrix
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ans = 0
+        if row1 == 0 and col1 == 0:
+            return self.matrix[row2][col2]
+        
+        if row1 == 0 and col1 != 0:
+            return self.matrix[row2][col2] - self.matrix[row2][col1-1]
+        
+        if row1 != 0 and col1 == 0:
+            return self.matrix[row2][col2] - self.matrix[row1-1][col2]
+        
+        if row1 != 0 and col1 != 0:
+            return self.matrix[row2][col2] - self.matrix[row2][col1-1] - self.matrix[row1-1][col2] + self.matrix[row1-1][col1-1]
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
