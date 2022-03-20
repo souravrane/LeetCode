228d7ce3e9bc9119b07811308aef71f8d9a65b20
Sourav Rane <sourav1121997@gmail.com>
2022-03-20 18:53:12 +0530

    Time: 242 ms (22.56%), Space: 126.5 MB (65.24%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,45 +1,53 @@
-class NumMatrix:
-
-    def __init__(self, matrix: List[List[int]]):
-        rows = len(matrix)
-        columns = len(matrix[0])
-        
-        # row wise prefix
-        for i in range(rows):
-            for j in range(1,columns):
-                matrix[i][j] += matrix[i][j-1]
-        
-        #column wise prefix
-        for i in range(columns):
-            for j in range(1,rows):
-                matrix[j][i] += matrix[j-1][i]
-                
-        self.matrix = matrix
-        
-
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = 0
-        
-        ans += self.matrix[row2][col2]
-        
-        if col1 > 0:
-            ans -= self.matrix[row2][col1-1]
-       
-        if row1 > 0:
-            ans -= self.matrix[row1-1][col2]
-            
-        if row1 > 0 and col1 > 0:
-            ans += self.matrix[row1-1][col1-1]
-      
-        return ans
+class NumMatrix {
     
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
+    int[][] prefixMatrix;
+    
+    public NumMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        
+        // prefix sum along rows
+        for(int i = 0; i < rows; i++){
+            for(int j = 1; j < columns; j++){
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+        
+        // prefix sum along columns
+        for(int i = 0; i < columns; i++){
+            for(int j = 1; j < rows; j++){
+                matrix[j][i] += matrix[j-1][i];
+            }
+        }
+        
+        prefixMatrix = matrix;
+        
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans = 0;
+        
+        ans +=prefixMatrix[row2][col2];
+        
+        if(col1 > 0){
+            ans -= prefixMatrix[row2][col1-1];
+        }
+        
+        if(row1 > 0){
+            ans -= prefixMatrix[row1-1][col2];
+        }
+        
+        if(row1 > 0 && col1 > 0){
+            ans += prefixMatrix[row1-1][col1-1];
+        }
+        
+        return ans;
+    }
+}
 
 
-
-
-
-
-
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
