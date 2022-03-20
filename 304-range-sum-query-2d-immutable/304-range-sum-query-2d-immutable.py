27f614e24c4582cba3a18014fb48251e9c5f6513
Sourav Rane <sourav1121997@gmail.com>
2022-03-20 18:27:05 +0530

    Time: 2064 ms (61.27%), Space: 24.8 MB (81.15%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -19,20 +19,27 @@ class NumMatrix:
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         ans = 0
-        if row1 == 0 and col1 == 0:
-            return self.matrix[row2][col2]
         
-        if row1 == 0 and col1 != 0:
-            return self.matrix[row2][col2] - self.matrix[row2][col1-1]
+        ans += self.matrix[row2][col2]
         
-        if row1 != 0 and col1 == 0:
-            return self.matrix[row2][col2] - self.matrix[row1-1][col2]
-        
-        if row1 != 0 and col1 != 0:
-            return self.matrix[row2][col2] - self.matrix[row2][col1-1] - self.matrix[row1-1][col2] + self.matrix[row1-1][col1-1]
-        
-
-
+        if col1 > 0:
+            ans -= self.matrix[row2][col1-1]
+       
+        if row1 > 0:
+            ans -= self.matrix[row1-1][col2]
+            
+        if row1 > 0 and col1 > 0:
+            ans += self.matrix[row1-1][col1-1]
+      
+        return ans
+    
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+
+
+
+
+
+
