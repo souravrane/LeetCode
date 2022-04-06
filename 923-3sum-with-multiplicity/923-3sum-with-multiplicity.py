427ccba3685d55712148e555cb0d426cae20a188
Sourav Rane <sourav1121997@gmail.com>
2022-04-06 17:47:01 +0530

    Time: 188 ms (48.26%), Space: 14 MB (92.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -2,27 +2,47 @@ from collections import defaultdict
 
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
-        ans = 0
-        m = 1000000007
+        '''
+        In this problem we look for 3 such triplets which satisfy the below conditions
         
-        count = [0]*101
-        for num in arr: count[num] += 1
-             
-        for i in range(101):
-            for j in range(i,101):
-                k = target - i - j
+        1) a == b == c
+            In this case our ans would be nC3 , where n is total frequency of a,b,c
+        
+        2) a == b != c
+            In this case our ans would be nC2 * frequency(c) where n is the frequency of a and b
+        
+        3) a != b != c
+            In this case our ans would be frequency(a) * frequency(b) * frequency(c)
+            
+        
+        The constraint 0 <= arr[i] <= 100 makes the problem easier for us.
+            
+        '''
+
+        m = (int)(10**9 + 7)
+        ans = 0
+        
+        freq = [0]*101
+        for num in arr: freq[num] += 1
+        
+        for a in range(101):
+            for b in range(a,101):
+                c = target - a - b
                 
-                if k < 0 or k > 100: continue
+                if c < 0 or c > 100: continue
                 
-                if i == j and j == k :
-                    ans = ans + count[i]*(count[i]-1)*(count[i]-2) // 6
+                if a == b and b == c:
+                    ans += (freq[a] * (freq[a]-1) * (freq[a]-2)) // 6 
                     
-                elif i == j and j != k:
-                    ans = ans + count[i]*(count[i]-1)*count[k] // 2
-                
-                elif i < j and j < k:
-                    ans = ans + count[i]*count[j]*count[k]
+                elif a == b and b != c:
+                    ans += (freq[a] * (freq[a]-1) * (freq[c])) // 2
+                    
+                elif a < b and b < c:
+                    ans += freq[a] * freq[b] * freq[c]
                 
                 ans = ans % m
-
+        
         return ans
+
+                
+                
