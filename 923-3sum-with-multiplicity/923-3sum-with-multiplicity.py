64e82c9652799c2fee6eaf9a9a06f2920f81efb3
Sourav Rane <sourav1121997@gmail.com>
2022-04-06 11:50:29 +0530

    Time: 203 ms (47.09%), Space: 14.1 MB (62.21%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        ans = 0
+        m = 1000000007
+        
+        count = [0]*101
+        for num in arr: count[num] += 1
+             
+        for i in range(101):
+            for j in range(i,101):
+                k = target - i - j
+                
+                if k < 0 or k > 100: continue
+                
+                if i == j and j == k :
+                    ans = ans + count[i]*(count[i]-1)*(count[i]-2) // 6
+                    
+                elif i == j and j != k:
+                    ans = ans + count[i]*(count[i]-1)*count[k] // 2
+                
+                elif i < j and j < k:
+                    ans = ans + count[i]*count[j]*count[k]
+
+        return ans % m
