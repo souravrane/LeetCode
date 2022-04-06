494e251894b3191107cb9c7efdae14d6193329d8
Sourav Rane <sourav1121997@gmail.com>
2022-04-06 22:13:17 +0530

    Time: 477 ms (5.76%), Space: 15.4 MB (9.09%) - LeetHub

---
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isNotSorted(self, index, num, arr):
+        if index == 0:
+            return num > arr[index + 1]
+        if index == len(arr)-1:
+            return num < arr[index - 1]
+        return (num < arr[index - 1]) or (num > arr[index + 1])
+    
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        minVal = float('inf')
+        maxVal = float('-inf')
+        
+        n = len(nums)
+        
+        if n == 1: return 0
+        
+        for i in range(n):     
+            curr = nums[i]
+            
+            if self.isNotSorted(i, curr, nums):
+                minVal = min(minVal, curr)
+                maxVal = max(maxVal, curr)  
+
+        
+        if minVal == float('inf'): return 0
+        
+        leftIdx = 0
+        for i in range(n):
+            if nums[i] > minVal:
+                leftIdx = i
+                break
+        
+        rightIdx = n-1
+        for j in range(n-1,-1,-1):
+            if nums[j] < maxVal:
+                rightIdx = j
+                break
+        
+        return rightIdx - leftIdx  + 1
+        
+        
+        
