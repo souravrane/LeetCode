0a012d0437ad8a4f870d4cfa902f146fdbf7bb86
Sourav Rane <sourav1121997@gmail.com>
2022-04-13 20:51:59 +0530

    Time: 95 ms (99.22%), Space: 21.2 MB (90.40%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -1,27 +1,12 @@
-'''
-How to generate gray codes ?
-N = 0  [0]
-N = 1 => (2^0) = (01) now add 01 to all the elements in reverse order and append those values to the list : [00,01]
-       
-N = 2 => (2^1) = (10) repeat sanme as above : [00,01, 11, 10]
-
-N = 3 => (2^2) = (100) : [00, 01, 11, 10, 110, 111, 101, 100]
-
-and so on...
-
-'''
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
+    def grayCode(self, n: int, res=[0,1]) -> List[int]:
+        if n == 1: return res
         
-        if n == 0:
-            return [0]
+        gc = self.grayCode(n-1,res)
         
-        res = self.grayCode(n-1)
-        
-        x = 1 << (n-1)
         temp = []
-        for num in res[::-1]:
-            temp.append(num+x)
+        for x in gc[::-1]:
+            temp.append(1<<(n-1) | x)
+        gc = gc + temp
         
-        return res + temp
-        
+        return gc
