944b514b603f1f48461c0332c8479d8cb57b8962
Sourav Rane <sourav1121997@gmail.com>
2022-06-02 18:35:49 +0530

    Time: 130 ms (22.25%), Space: 14.8 MB (16.92%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,14 +1,9 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        r = len(matrix)
-        c = len(matrix[0])
+        m,n=len(matrix),len(matrix[0])
+        ans = [[None] * m for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+                ans[j][i]=matrix[i][j]
         
-        res = []
-        
-        for i in range(c):
-            row = []
-            for j in range(r):
-                row.append(matrix[j][i])
-            res.append(row)
-        
-        return res
+        return ans
