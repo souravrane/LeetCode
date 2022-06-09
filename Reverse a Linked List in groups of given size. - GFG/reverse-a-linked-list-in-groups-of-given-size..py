3200a0a6a03ed8e7b1c8f1a20dd5d18430d70fea
Sourav Rane <sourav1121997@gmail.com>
2022-06-09 19:25:24 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..py
@@ -0,0 +1,102 @@
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def getKth(self, cur, k):
+        while cur and k:
+            cur = cur.next
+            k -= 1
+        return cur
+        
+    
+    def reverse(self,head, k):
+        dummy = Node(-1)
+        dummy.next = head
+        
+        prevGroup = dummy
+        while True:
+            kthNode = self.getKth(prevGroup, k)
+            
+            if kthNode == None:
+                prev = None
+                cur = prevGroup.next
+        
+                while cur:
+                    temp = cur.next
+                    cur.next = prev
+                    prev = cur
+                    cur = temp
+                
+                prevGroup.next = prev
+                break
+            
+            nextGroup = kthNode.next
+            
+            prev = kthNode.next
+            cur = prevGroup.next
+            
+            while cur != nextGroup:
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+            
+            temp = prevGroup.next
+            prevGroup.next = prev
+            prevGroup = temp
+        
+        return dummy.next
+#{ 
+#  Driver Code Starts
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # self.tail
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = input()
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        k = int(input())
+        new_head = LinkedList()
+        ob=Solution()
+        new_head = ob.reverse(llist.head, k)
+        llist.head = new_head
+        llist.printList()
+        t -= 1
+
+
+
+
+# } Driver Code Ends
