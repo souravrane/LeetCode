12004def0c5af83b34e83e5f9234e18534f326ac
Sourav Rane <sourav1121997@gmail.com>
2022-06-14 00:38:38 +0530

    Added solution - LeetHub

---
+++ Parenthesis Checker - GFG/parenthesis-checker.py
@@ -0,0 +1,61 @@
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if brackets are balanced or not.
+    def ispar(self,x):
+        # code here
+        m = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        stack = []
+        for ch in x:
+            if ch in ['(','[', '{']:
+                stack.append(ch)
+            else:
+                if stack and stack[-1] == m[ch]:
+                    stack.pop()
+                else:
+                    return False
+                    
+        if stack: return False
+        return True
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        #n = int(input())
+        #n,k = map(int,imput().strip().split())
+        #a = list(map(int,input().strip().split()))
+        s = str(input())
+        obj = Solution()
+        if obj.ispar(s):
+            print("balanced")
+        else:
+            print("not balanced")
+# } Driver Code Ends
