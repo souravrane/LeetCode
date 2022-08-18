b8d750e290d28e0f8a74b8087d80cc2e8f6689b2
Sourav Rane <sourav1121997@gmail.com>
2022-08-18 23:18:37 +0530

    Time: 37 ms (81.69%), Space: 13.8 MB (65.83%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,28 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.res = [[1],[1,1]]
-        
     def generate(self, numRows: int) -> List[List[int]]:
+        pTriangle = [[1],[1,1]]
+        for i in range(3,numRows+1):
+            row = [1]
+            last = pTriangle[-1]
+            for i in range(len(last)-1):
+                row.append(last[i] + last[i+1])
+            row.append(1)
+            pTriangle.append(row)
         
-        def generateTriangle(arr,rows):
-            if rows == 0:
-                return
+        return pTriangle[:numRows]
             
-            newRow = [1]
-            for i in range(1,len(arr)):
-                newRow.append(arr[i]+arr[i-1])
-            newRow.append(1)
-            self.res.append(newRow)
-            
-            return generateTriangle(newRow,rows-1)
-            
-        if numRows == 1:
-            return self.res[:1]
-        
-        if numRows == 2:
-            return self.res[:2]
-        
-        generateTriangle([1,1],numRows-2)
-        
-        return self.res
-        
