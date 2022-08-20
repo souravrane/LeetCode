89053377c4d4cb378a7d7d89b489d969ed5611d3
Sourav Rane <sourav1121997@gmail.com>
2022-08-20 12:43:45 +0530

    Time: 49 ms (88.76%), Space: 14.4 MB (89.28%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,28 +1,15 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-        O(M+N)
-        As they are row and column wise sorted we can start traversing from top right corner
-        if corner is smaller than the element then we move downwards to the next row
-        if corner is greater than the element then we move leftwords to the previous column
-        This way we will reach our element and return true or exit the matrix bounds and return false
+        r = len(matrix)
+        c = len(matrix[0])
         
-        '''
-        rows = len(matrix)
-        columns = len(matrix[0])
-        
-        i,j = 0,columns-1
-        
-        while i < rows and j >= 0:
-            
-            if matrix[i][j] == target:
-                return True
-            
-            if target > matrix[i][j]:
-                i += 1
-            else:
+        i,j = 0,c-1
+        while i < r and j >= 0:
+            if target < matrix[i][j]:
                 j -= 1
+            elif target > matrix[i][j]:
+                i += 1
+            else: return True
         
         return False
-        
         
