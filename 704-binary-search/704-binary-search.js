a1fbdcc794e8e543cabf14d614bb24dde52e5486
Sourav Rane <sourav1121997@gmail.com>
2022-09-05 11:33:35 +0530

    Time: 66 ms (95.54%), Space: 45.1 MB (44.66%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+    while(left <= right){
+        const mid = left + Math.floor((right - left) / 2);
+        if(nums[mid] === target) return mid
+        if(nums[mid] > target)
+            right = mid - 1
+        else
+            left = mid + 1
+    }
+    return -1
+};
