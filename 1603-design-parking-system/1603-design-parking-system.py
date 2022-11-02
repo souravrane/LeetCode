f367b0a1aeeafe161a4e735cf97cea1f9b8748fa
Sourav Rane <sourav1121997@gmail.com>
2022-11-02 17:06:38 +0530

    Time: 140 ms (93.75%), Space: 14.5 MB (63.17%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,6 +1,6 @@
 class ParkingSystem:
     def __init__(self, big: int, medium: int, small: int):
-        self.parkingSpace = {1:big, 2:medium, 3:small}
+        self.parkingSpace = ['_',big,medium,small]
 
     def addCar(self, carType: int) -> bool:
         if self.isParkingAvailable(carType):
