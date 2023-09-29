bda85e8f3c27a0cbf6bce59479026e056d632b64
Sourav Rane <sourav1121997@gmail.com>
2023-09-30 02:00:05 +0530

    Time: 869 ms (24.52%) | Memory: 30.5 MB (57.11%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        increasing, decreasing = True, True
+        for i in range(1, len(nums)):
+            if(nums[i-1] > nums[i]): increasing = False
+            if(nums[i-1] < nums[i]): decreasing = False
+        return increasing or decreasing
