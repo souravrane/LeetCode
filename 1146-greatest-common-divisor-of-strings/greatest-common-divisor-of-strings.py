3f901993d7861998909975e9f4e2593f822cf122
Sourav Rane <sourav1121997@gmail.com>
2023-12-18 08:05:57 +0530

    Time: 41 ms (52.01%) | Memory: 16.2 MB (64.31%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,19 +1,5 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        result = ''
-        i, j = 0, 0
-        common = []
-        
-        while(i < len(str1) and j < len(str2)):
-            if(str1[i] == str2[j]):
-                common.append(str1[i])
-                if(len(str1) % len(common) == 0 and len(str2) % len(common) == 0):
-                    result = ''.join(common)
-                i += 1
-                j += 1
-            else:
-                break
-        
-        if(str1.count(result)*len(result) == len(str1) and str2.count(result)*len(result) == len(str2)): return result
-        return ''
-         
+        if str1 + str2 != str2 + str1: return ""
+        maxGcd = gcd(len(str1), len(str2))
+        return str1[:maxGcd]
