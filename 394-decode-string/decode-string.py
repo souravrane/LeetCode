41a99b570c854d1b3fb33bbbd01515e9cadb8be7
Sourav Rane <sourav1121997@gmail.com>
2023-12-28 11:58:17 +0530

    Time: 49 ms (6.38%) | Memory: 17.3 MB (5.95%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,27 +1,22 @@
 class Solution:
-    def _extractText(self, stack):
-        text = []
-        while(stack and stack[-1] != "["):
-            char = stack.pop()
-            text.append(char)
-        if(stack): stack.pop()
-        return ''.join(text[::-1])
-
-    def _extractNum(self, stack):
-        exp, value = 0, 0
-        while(stack and stack[-1].isnumeric()):
-            num = stack.pop()
-            value += int(num)*pow(10,exp)
-            exp += 1
-        return value
-        
     def decodeString(self, s: str) -> str:
         stack = list()
         for char in s:
-            if(char == "]"):
-                newSub = self._extractText(stack)
-                number = self._extractNum(stack)
-                stack.append(newSub*number)
+            if char == ']':
+                text = list()
+                while stack[-1] != '[':
+                    text.append(stack.pop())
+                text.reverse()
+                stack.pop()
+                
+                number = list()
+                while stack and stack[-1].isnumeric():
+                    number.append(stack.pop())
+                
+                number.reverse()
+                finalText = ''.join(text)*int(''.join(number))
+                stack.append(finalText)
             else:
                 stack.append(char)
         return ''.join(stack)
+        
