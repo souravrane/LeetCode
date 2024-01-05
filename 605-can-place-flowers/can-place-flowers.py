d2ea217c4c733b6f21510511e7488c8eb2d74661
Sourav Rane <sourav1121997@gmail.com>
2024-01-06 02:50:26 +0530

    Time: 140 ms (81.47%) | Memory: 17.6 MB (26.43%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,29 +1,29 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        '''
+        1000
+        0000
+
+        0001
+        10001
+        '''
+  
         start = -1
         count = 0
         result = 0
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0: count += 1
             else:
-                if start == -1:
-                    result += floor(count / 2)
-                else:
+                if start > -1 :
                     result += floor((count - 1) / 2)
-                    
-                start = i
-                print(flowerbed[start:i+1], count)
+                else:
+                    result += floor(count / 2)
                 count = 0
+                start = i
         
-        if start > -1 and count != 0: result += floor(count / 2)
+
+        if start != -1 and count != 0: result += floor(count / 2)
         else: result += ceil(count / 2)
         return result >= n
-    '''
-    1 - 0
-    2 - 0
-    3 - 1
-    4 - 1
-    5 - 2
-    6 - 2
 
-    '''
+                
