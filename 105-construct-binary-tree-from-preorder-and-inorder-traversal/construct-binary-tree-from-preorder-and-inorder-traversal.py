e4a1f5ccae99ab13069b8ef321bc6b886dcaa201
Sourav Rane <sourav1121997@gmail.com>
2024-01-11 19:23:42 +0530

    Time: 56 ms (92.01%) | Memory: 19.1 MB (92.50%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def helper(self, pre, i, j, ino, x, y):
         if i > j: return None
@@ -12,11 +6,7 @@ class Solution:
         root = TreeNode(pre[i])
         
         # find the root rootIdx
-        rootIdx = 0
-        for _ in range(len(ino)):
-            if ino[_] == pre[i]:
-                rootIdx = _
-                break
+        rootIdx = self.inMap[pre[i]]
 
         # create left and right subtrees
         left = self.helper(pre, i + 1, i + (rootIdx - x), ino, x, rootIdx - 1)
@@ -26,8 +16,8 @@ class Solution:
         root.right = right
         return root
 
-
-
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         n = len(preorder)
+        self.inMap = dict()
+        for idx,num in enumerate(inorder): self.inMap[num] = idx
         return self.helper(preorder, 0, n-1, inorder, 0, n-1)
