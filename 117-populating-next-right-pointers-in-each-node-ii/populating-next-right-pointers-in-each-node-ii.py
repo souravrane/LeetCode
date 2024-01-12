bca1b66a61adfa6e003fdb6b88c4788153a731a3
Sourav Rane <sourav1121997@gmail.com>
2024-01-12 23:08:12 +0530

    Time: 42 ms (90.00%) | Memory: 18.5 MB (13.91%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -3,11 +3,10 @@ class Solution:
         if not root: return root
         dq = deque([root])
         while dq:
-            for i in range(1,len(dq)):
-                dq[i-1].next = dq[i]
-
+            qLen = len(dq)
             for i in range(len(dq)):
                 node = dq.popleft()
                 if node.left: dq.append(node.left)
                 if node.right: dq.append(node.right)
+                if i != qLen - 1: node.next = dq[0]
         return root            
