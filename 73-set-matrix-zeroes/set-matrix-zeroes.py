a51b9e5b5a883b1010877384ffeec06aa3912992
Sourav Rane <sourav1121997@gmail.com>
2025-10-21 11:28:52 +0530

    Time: 2 ms (76.91%) | Memory: 18.4 MB (53.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,21 +1,33 @@
 class Solution:
-    def changeRowCol(self, mat, r, c):
-        for i in range(len(mat[0])):
-            mat[r][i] = pow(2,31) if mat[r][i] != 0 else 0
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        row, col = len(matrix), len(matrix[0])
+        outer_most = set()
+
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    if i == 0 and j != 0: outer_most.add(1)
+                    if i != 0 and j == 0: outer_most.add(2)
+                    if i == 0 and j == 0: outer_most.update([1,2])
+                    matrix[0][j] = pow(2,31)
+                    matrix[i][0] = pow(2, 31)
         
-        for i in range(len(mat)):
-            mat[i][c] = pow(2,31) if mat[i][c] != 0 else 0
-
-
-    def setZeroes(self, mat: List[List[int]]) -> None:
-        r, c = len(mat), len(mat[0])
-
-        for i in range(r):
-            for j in range(c):
-                if mat[i][j] == 0:
-                    self.changeRowCol(mat, i, j)
+        #set rows
+        for i in range(1, row):
+            if matrix[i][0] == pow(2,31):
+                matrix[i][0] = 0
+                for j in range(1, col):
+                    matrix[i][j] = 0
         
-        for i in range(r):
-            for j in range(c):
-                if mat[i][j] == pow(2,31):
-                    mat[i][j] = 0            
+        # set cols
+        for j in range(1, col):
+            if matrix[0][j] == pow(2,31):
+                matrix[0][j] = 0
+                for i in range(1, row):
+                    matrix[i][j] = 0
+        
+        if 1 in outer_most:
+            for j in range(col): matrix[0][j] = 0
+        
+        if 2 in outer_most:
+            for i in range(row): matrix[i][0] = 0
