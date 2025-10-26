543ae7999e6823bb917b1b423b09a4361dbc39c7
Sourav Rane <sourav1121997@gmail.com>
2025-10-26 21:49:24 +0530

    Time: 11 ms (90.37%) | Memory: 17.9 MB (54.07%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,8 +2,10 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        freqMagzine = Counter(magazine)
-        freqRansom = Counter(ransomNote)
-        for letter in freqRansom:
-            if freqMagzine[letter] < freqRansom[letter]: return False
-        return True      
+        r_count = Counter(ransomNote)
+        m_count = Counter(magazine)
+        
+        for k in r_count.keys():
+            if k not in m_count or (r_count[k] > m_count[k]): return False
+        
+        return True
