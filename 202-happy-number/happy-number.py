d0e4a6d0226da9f1a4d68a6fd0de8822d08f01f0
Sourav Rane <sourav1121997@gmail.com>
2025-10-27 14:46:23 +0530

    Time: 4 ms (15.24%) | Memory: 17.6 MB (86.86%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,12 +1,14 @@
 class Solution:
+    def get_next(self, num):
+        result = 0
+        while num > 0:
+            num,r = divmod(num, 10)
+            result += r**2
+        return result
+
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n > 1:
-            num = str(n)
-            ans = 0
-            for x in num: ans += int(x)**2
-            if  ans in seen: return False
-            seen.add(ans)
-            n = ans
-        return True
-        
+        slow, fast = n, self.get_next(n)
+        while fast != 1 and slow != fast:
+            slow = self.get_next(slow)
+            fast = self.get_next(self.get_next(fast))
+        return fast == 1
