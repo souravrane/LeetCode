6a6e57dc02b3f711ddf2f9a70ffed05ea4cd8f5f
Sourav Rane <sourav1121997@gmail.com>
2025-10-30 14:13:35 +0530

    Time: 103 ms (46.96%) | Memory: 51.4 MB (35.52%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -3,16 +3,18 @@ class Solution:
         # sorting it based on y coordinate, because thats where we will leave our arrows
         points = sorted(points, key = lambda x:x[1])
         
-        temp = [points[0]]
+        temp = points[0]
+        arrows = 1
 
         for i in range(1, len(points)):
             x1, y1 = points[i]
-            x2, y2 = temp[-1]
+            x2, y2 = temp
 
             if y2 >= x1:
                 x2 = max(x1, x2)
                 y2 = min(y1, y2)
             else:
-                temp.append([x1, y1])
+                arrows += 1
+                temp = points[i]
         
-        return len(temp)
+        return arrows
